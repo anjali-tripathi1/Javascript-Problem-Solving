687e64ca962eee4ce2ca66d76fd85400c7b5a08a
anjali-tripathi1 <anjalitripathi.kit@gmail.com>
2025-11-04 13:40:03 +0530

    upload

--- ProblemSolving/practice.js
+++ ProblemSolving/practice.js
@@ -1,10 +1,17 @@
-function checkEvenOdd (num) {
-    const rem = num % 2;
-    if(rem === 0){
-        return `${num} is Even.`
-    } 
-
-    return `${num} is Odd.`
+function sum (num) {
+   let str = num.toString().split('');
+   let red = str.reduce((acc, digit) => acc + parseInt(digit), 0);
+   return red;
 }
 
-console.log(checkEvenOdd('128'));
+console.log(sum(76));
+console.log(sum(5676));
+console.log(sum(567654));
+
+function table (tableof) {
+   for(let i = 1; i <= 10; i++){
+        console.log(tableof * i); 
+   }
+}
+
+table(8);
