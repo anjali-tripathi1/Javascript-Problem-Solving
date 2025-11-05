e0f4e58ef523ab31291a433fd7117c6e9345defe
anjali-tripathi1 <anjalitripathi.kit@gmail.com>
2025-11-05 08:17:39 +0530

    upload

--- ProblemSolving/practice.js
+++ ProblemSolving/practice.js
@@ -16,10 +16,37 @@ function table (tableof, tabletill) {
 
 table(7, 8)
 
-function bigNum (array) {
-     let biggest = array[0];
-     for(let i = 1; i < array.length; i++){
-        
-   
-     }
+function isPalindrome(str){
+    str = str.toLowerCase();
+    for(let i = 0; i < str.length; i++){
+         if(str[i] !== str[str.length - 1 - i]){
+              return `${str} is not palindrome.`
+         }
+    }
+
+    return `${str} is palindrome.`
 }
+
+console.log(isPalindrome('Anjali'));
+console.log(isPalindrome('software'));
+console.log(isPalindrome('Level'));
+console.log(isPalindrome('Madam'));
+
+
+function pow (base, exponent) {
+   let result  = 1
+    for(let i = 1; i <= exponent; i++){
+           result = result * base;
+    }
+
+    return result;
+}
+
+console.log(pow(2,6));
+
+function power (base, exponent) {
+    let po = Math.pow(base , exponent)
+    return po;
+} 
+
+console.log(power(7, 3));
